5df599d860c94c21bad941859236f9b55d95fb95
Fatih Can <ybsfatihcan@gmail.com>
2020-12-30 15:01:22 +0300

    animation

---
+++ app/src/main/java/com/example/androidxmlanimation/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.androidxmlanimation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val oneAnim = AnimationUtils.loadAnimation(this,R.anim.oneanim)
+        val twoAnim = AnimationUtils.loadAnimation(this,R.anim.twoanim)
+        val threeAnim = AnimationUtils.loadAnimation(this,R.anim.threeanim)
+        val fourAnim = AnimationUtils.loadAnimation(this,R.anim.fouranim)
+
+        val emailText = findViewById(R.id.editTextEmailAddress) as EditText
+        val passwordText = findViewById(R.id.editTextPassword) as EditText
+        val button = findViewById(R.id.button) as Button
+
+
+
+        val welcomeText = findViewById(R.id.textView) as TextView
+        val imageLogo = findViewById(R.id.imageView) as ImageView
+
+        welcomeText.startAnimation(oneAnim)
+        imageLogo.startAnimation(twoAnim)
+        emailText.startAnimation(threeAnim)
+        passwordText.startAnimation(threeAnim)
+        button.startAnimation(fourAnim)
+
+
+        val loginButton = findViewById<Button>(R.id.button)
+        loginButton.setOnClickListener {
+
+        }
+    }
+}
